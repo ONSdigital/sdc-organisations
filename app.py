21483d7a3508eea4427cf19948b50882dc384904
Nick Gravgaard <me@nickgravgaard.com>
2016-11-07 11:39:23 +0000

    Change default port to 5010 as specified in sdc-frontstage-ui

--- app.py
+++ app.py
@@ -282,5 +282,5 @@ if __name__ == '__main__':
     create_associations(organisations)
 
     # Start server
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get('PORT', 5010))
     app.run(debug=True, host='0.0.0.0', port=port)
